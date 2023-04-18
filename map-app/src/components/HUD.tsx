2879a80695b76368962ad36a2007b10626e73588
Maryanne Wachter <maryanne.wachter@gmail.com>
2023-04-18 07:30:39 -0700

    Clean up HUD fields and fix overflow display

--- map-app/src/components/HUD.tsx
+++ map-app/src/components/HUD.tsx
@@ -1,9 +1,36 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
 import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+
+const desiredFields = new Set([
+  "status",
+  "zone",
+  "permit_category",
+  "permit_sub_type",
+  "work_description",
+  "council_district",
+  "contractors_business_name",
+  "issue_date",
+  "status_date",
+  "log_id",
+]);
+
+const getStatus = (dbStatus: string) => {
+  if (dbStatus === "retrofit") {
+    return "Retrofit in progress / completed";
+  }
+  if (dbStatus === "not retrofit") {
+    return "No retrofit permit found in LADBS";
+  }
+  if (dbStatus === "retrofit not required") {
+    return "Retrofit assessed and not required";
+  }
+};
 
 const HUD = ({ rawData }) => {
   const data = JSON.parse(rawData.data);
@@ -13,28 +40,30 @@ const HUD = ({ rawData }) => {
       <>
         {Object.entries(item).map(([key, value], i) => {
           const formattedKey = key.split("_").join(" ");
-          return (
-            <>
-              <div>
-                <Typography
-                  key={`${key}_${i}`}
-                  display="inline"
-                  fontWeight="700"
-                  variant="button"
-                >
-                  {formattedKey}:
-                </Typography>
-                <span>{` `}</span>
-                <Typography
-                  key={`${value}_${i}`}
-                  display="inline"
-                  variant="body2"
-                >
-                  {value}
-                </Typography>
-              </div>
-            </>
-          );
+          if (desiredFields.has(key)) {
+            return (
+              <>
+                <div>
+                  <Typography
+                    key={`${key}_${i}`}
+                    display="inline"
+                    fontWeight="700"
+                    variant="button"
+                  >
+                    {formattedKey}:
+                  </Typography>
+                  <span>{` `}</span>
+                  <Typography
+                    key={`${value}_${i}`}
+                    display="inline"
+                    variant="body2"
+                  >
+                    {value}
+                  </Typography>
+                </div>
+              </>
+            );
+          }
         })}
       </>
     );
@@ -45,32 +74,55 @@ const HUD = ({ rawData }) => {
         <Box
           sx={{
             position: "absolute",
-            minWidth: 300,
-            maxWidth: { md: "50%", sm: "80%" },
-            minWidth: 300,
-            maxHeight: "50%",
-            overflowY: "auto",
+            minWidth: "50vw",
+            maxWidth: { md: "50vw", sm: "80vw" },
             top: 80,
+            left: 56,
+            right: 56,
           }}
         >
-          <Card square sx={{ opacity: 0.8, px: 2 }}>
-            <CardContent>
+          <Paper square sx={{ opacity: 0.8, px: 2, py: 2, maxHeight: "80vh" }}>
+            <div>
+              <Typography
+                variant="h5"
+                sx={{ fontWeight: 700, fontVariant: "small-caps" }}
+              >
+                {rawData.address.split(/\d\d\d\d\d/)[0].toLowerCase()}
+              </Typography>{" "}
               <Typography variant="h6" sx={{ fontWeight: 700 }}>
-                {rawData.address.split(/\d\d\d\d\d/)[0]}
-              </Typography>
-              <Typography variant="h7" sx={{ fontWeight: 700 }}>
                 {rawData.address.match(/\d\d\d\d\d/)[0]}
               </Typography>
-              {data.map((item) => {
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: 700, fontVariant: "small-caps" }}
+              >
+                Status: {getStatus(rawData.retrofit_status)}
+              </Typography>
+            </div>
+            <div style={{ "padding-bottom": 8 }}>
+              <Typography
+                variant="h6"
+                sx={{ fontVariant: "small-caps", fontWeight: 700 }}
+              >
+                Database Entries
+              </Typography>
+            </div>
+            <Box
+              sx={{
+                maxHeight: "30vh",
+                overflowY: "auto",
+              }}
+            >
+              {data.map((item, i) => {
                 return (
                   <>
-                    <Divider />
                     {formattedEntry(item)}
+                    {!(i === data.length - 1) && <Divider />}
                   </>
                 );
               })}
-            </CardContent>
-          </Card>
+            </Box>
+          </Paper>
         </Box>
       </Container>
     </Box>
